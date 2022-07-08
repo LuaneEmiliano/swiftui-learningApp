ad7a49fc15ce3677c1d757f8b361a75654ca7889
luane <luaneemiliano@gmail.com>
2022-07-08 07:26:31 -0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by luane Niejelski on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
